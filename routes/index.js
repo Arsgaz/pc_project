8b0675e669303865661fa107d3a3ea2f38bc5f1b
Arsgaz <petrov.ars.i.23@gmail.com>
2024-10-27 16:13:56 +0300

    Пункт 4_4

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/cpu', function(req, res, next) {
-  res.render('cpu', {
+  res.render('parts', {
     title: "Процессор",
     picture: "images/cpu.jpg",
     desc: "Процессор служит мозгом компьютера"
     });
 });
 router.get('/vc', function(req, res, next) {
-  res.render('vc', {
+  res.render('parts', {
     title: "Видеокарта",
     picture: "images/vc.jpg",
     desc: "Видеокарта обрабатывает изображение"
     });
 });
 router.get('/mb', function(req, res, next) {
-  res.render('mb', {
+  res.render('parts', {
     title: "Материнская плата",
     picture: "images/mb.jpg",
     desc: "Материнская плата является связующим звеном между всеми компонентами компьютера"
