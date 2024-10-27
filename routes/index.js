f7f2d680c7388ced7d1e00642f50fd78b29481d2
Arsgaz <petrov.ars.i.23@gmail.com>
2024-10-27 15:27:14 +0300

    Пункт 4_3

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/cpu', function(req, res, next) {
-  res.send("<h1>Процессор</h1>")
+  res.render('cpu', {
+    title: "Процессор",
+    picture: "images/cpu.jpg",
+    desc: "Процессор служит мозгом компьютера"
+    });
 });
 router.get('/vc', function(req, res, next) {
-  res.send("<h1>Видеокарта</h1>")
+  res.render('vc', {
+    title: "Видеокарта",
+    picture: "images/vc.jpg",
+    desc: "Видеокарта обрабатывает изображение"
+    });
 });
 router.get('/mb', function(req, res, next) {
-  res.send("<h1>Материнская плата</h1>")
+  res.render('mb', {
+    title: "Материнская плата",
+    picture: "images/mb.jpg",
+    desc: "Материнская плата является связующим звеном между всеми компонентами компьютера"
+    });
 });
 module.exports = router;
