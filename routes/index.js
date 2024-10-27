9f1a0a34d058b901cfffc65897ce5b20ee5f5e78
Arsgaz <petrov.ars.i.23@gmail.com>
2024-10-27 15:12:10 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/cpu', function(req, res, next) {
+  res.send("<h1>Процессор</h1>")
 });
-
+router.get('/vc', function(req, res, next) {
+  res.send("<h1>Видеокарта</h1>")
+});
+router.get('/mb', function(req, res, next) {
+  res.send("<h1>Материнская плата</h1>")
+});
 module.exports = router;
