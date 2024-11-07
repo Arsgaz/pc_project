0efaa4665ef1dd2b2d28a83d2f88d5d0972ecf07
Arsgaz <petrov.ars.i.23@gmail.com>
2024-11-07 11:28:36 +0300

    Пункт 7_3

---
+++ models/part.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var partSchema = new Schema({
+title: String,
+nick: {
+type: String,
+unique: true,
+required: true
+},
+avatar: String,
+desc: String,
+created:{
+type:Date,
+default:Date.now
+}
+})
+module.exports.Part = mongoose.model("Part", partSchema)
--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.meow = function(){
-console.log(this.name + " сказал мяу")
-}
-const Cat = mongoose.model('Cat', schema);
-const kitty = new Cat({ name: 'Пушок' });
-kitty.save().then(() => kitty.meow());
+var Part = require('./models/part.js').Part
+var part = new Part({
+title: "Процессор",
+nick: "cpu",
+})
+part.save();
