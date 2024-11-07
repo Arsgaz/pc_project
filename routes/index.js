f874baf061f9a674b50db3d07698523eac20b1f8
Arsgaz <petrov.ars.i.23@gmail.com>
2024-11-07 12:58:08 +0300

    Пункт 8_4

--- routes/index.js
+++ routes/index.js
@@ -6,25 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   });
 
-router.get('/cpu', function(req, res, next) {
-  res.render('parts', {
-    title: "Процессор",
-    picture: "images/cpu.jpg",
-    desc: "Процессор служит мозгом компьютера"
-    });
-});
-router.get('/vc', function(req, res, next) {
-  res.render('parts', {
-    title: "Видеокарта",
-    picture: "images/vc.jpg",
-    desc: "Видеокарта обрабатывает и выводит изображение"
-    });
-});
-router.get('/mb', function(req, res, next) {
-  res.render('parts', {
-    title: "Материнская плата",
-    picture: "images/mb.jpg",
-    desc: "Материнская плата является связующим звеном между всеми компонентами компьютера"
-    });
-});
 module.exports = router;
