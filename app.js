98f5fa0c114c8e3b9ee52e79eb113ab964dd7a3d
Arsgaz <petrov.ars.i.23@gmail.com>
2024-11-09 18:25:57 +0300

    Пункт 9_3

--- app.js
+++ app.js
@@ -24,13 +24,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-    secret: "PC",
-    cookie:{maxAge:60*1000},
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-  }))
+var MongoStore = require('connect-mongo');
+  app.use(session({
+  secret: "PC",
+  cookie:{maxAge:60*1000},
+  proxy: true,
+  resave: true,
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/pc'})
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
